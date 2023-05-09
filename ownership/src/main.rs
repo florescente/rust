8693ef93f1bdb0c1e412441f54d25fc6692d5cae
florescente <campoflorescente@gmail.com>
2023-05-09 18:22:34 -0300

    rule one

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -6,11 +6,33 @@
 
 // Rules of References
 
-//At any given time, you can have either one mutable reference or any number of immutable references.
-//References must always be valid.
+// At any given time, you can have either one mutable reference or any number of immutable references.
+// References must always be valid.
 
 
 
 fn main() {
-    let s = String::From("String");
+    let s1 = String::from("hello");
+    // let s2 = s1;
+    // this gives an error because the reference was passed from s1 to s2
+    let s2 = s1.clone();
+
+    println!("{}, world {}!", s1, s2);
+
+    let x = 5;
+    let y = x;
+
+    println!("x = {}, y = {}", x, y);
+
+    // this doesn't give an error because the size of intergers is known at compile time
+    // so the value is simply copied to the y variable
+
+    // types with copy trait: 
+
+    // All the integer types, such as u32.
+    // The Boolean type, bool, with values true and false.
+    // All the floating-point types, such as f64.
+    // The character type, char.
+    // Tuples, if they only contain types that also implement Copy. For example, (i32, i32) implements Copy, but (i32, String) does not.
+
 }
